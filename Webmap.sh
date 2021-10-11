ee1784950eb5a3254dbf303742b2fd02071202ca
Iuri Moro <iuribpmoro@gmail.com>
2021-10-11 14:27:23 -0300

    Updated grep url on scanSubs

--- Webmap.sh
+++ Webmap.sh
@@ -148,7 +148,7 @@ createDirectories() {
 getSubs() {
 	echo "[+] Harvesting subdomains with assetfinder..."
 	assetfinder $url >> $url/recon/assets.txt
-	cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
+	cat $url/recon/assets.txt | grep $url >> $url/recon/final.txt
 	rm $url/recon/assets.txt
 
 	echo "[+] Double checking for subdomains with amass..."
