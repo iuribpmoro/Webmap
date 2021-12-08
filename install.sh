4066e87622355da4807eecad91437e670ba45875
Iuri Moro <iuribpmoro@gmail.com>
2021-12-08 13:51:20 -0300

    Fixed chrome installation

--- install.sh
+++ install.sh
@@ -237,7 +237,7 @@ installGowitness () {
 installChrome () {
     echo "${bold}Installing Google Chrome...${reset}"
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo apt install ./google-chrome-stable_current_amd64.deb -Y
+    sudo apt install ./google-chrome-stable_current_amd64.deb -y
     rm ./google-chrome-stable_current_amd64.deb
 
     commandResult=$?
